d745563e9fa04ea5e02f5109c8cd8e96a4f6e954
Matt Hunt <wmatthunt@gmail.com>
2021-02-16 14:10:20 -0500

    initial commit

---
+++ Game/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Dotnet RPG");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}!");
+        }
+    }
+}
