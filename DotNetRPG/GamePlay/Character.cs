efd704d01f928eec8fe47702f229e4d2986dbd2f
Matt Hunt <wmatthunt@gmail.com>
2021-02-19 19:39:12 -0500

    made character class

--- DotNetRPG/GamePlay/Character.cs
+++ DotNetRPG/GamePlay/Character.cs
@@ -6,9 +6,51 @@ namespace DotNetRPG
 {
     class Character
     {
-        public Character()
-        {
+        //Core 
+        private String name = "";
+        private int level = 0;
+        private int attributePoints = 3;
+        private int exp = 0;
+        private int expMax = 100;
+        //Attributes (will change these)
+        private int vitality = 1;
+        private int strength = 1;
+        private int dexterity = 1;
+        private int agility = 1;
+        private int intelligence = 1;
 
+        //Stats
+        private int hp = 0;
+        private int hpMax = 0;
+        private int damage = 0;
+        private int damageMax = 0;
+        private int accuracy = 0;
+        private int defense = 0;
+
+        //General
+        int gold = 100;
+    
+        private void CalculateExp()
+        {
+            this.expMax = level * 100;
+        }
+        private void CalculateStats()
+        {
+            this.hp = this.vitality*10;
+            this.damageMax = this.strength + 2;
+            this.damage = this.strength;
+            this.accuracy = this.dexterity * 2;
+            this.defense = this.agility * 2;
+
+        }
+        public Character(String name)
+        {
+            this.CalculateStats();
+            this.name = name;
+        }
+        public override String ToString()
+        {
+            return this.name;
         }
     }
 }
--- DotNetRPG/States/StateMainMenu.cs
+++ DotNetRPG/States/StateMainMenu.cs
@@ -6,12 +6,15 @@ namespace DotNetRPG
 {
     class StateMainMenu : State
     {
+        Character character;
         public StateMainMenu(Stack<State> states) 
         : base(states)
         {
             //Console.WriteLine("Hello from the Main Menu.");
             //had to remove this
             //states.Push(new StateGame(states));
+             this.character = new Character("Hero");
+            Console.WriteLine(character.ToString());
         }
          override public void Update()
         {
